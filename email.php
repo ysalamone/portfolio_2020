b4697d832b282d04e0b84313da5be67533539438
Yohan Salamone <yohan.salamone@outlook.com>
2021-02-15 14:46:29 +0100

    formulaire message - essai nº2 (corps HTML)

--- email.php
+++ email.php
@@ -5,24 +5,23 @@ echo var_dump($_POST);
 $to = 'Yohan Salamone <yohan.salamone@outlook.com>';
 $subject = 'Nouveau message de la part de '.$_POST['nom'];
 $message =
-    'Bonjour,' . "\r\n" .
-    'Un message a été envoyé depuis le formulaire du portfolio, le voici : ' . "\r\n" .
-    "\r\n" .
-    '----------------------------------------------------------------------' . "\r\n" .
-    'De la part de : '.$_POST['nom'].'<'.$_POST['email'].'>' . "\r\n" .
-    'Date d\'envoi : '.date("\l\\e d/m/Y à H:i:s") . "\r\n" .
-    'Sujet : '.$_POST['sujet']. "\r\n" .
-    'Message : '.$_POST['message']. "\r\n" .
-    '----------------------------------------------------------------------' . "\r\n" .
-    "\r\n" .
-    'Bonne journée.'. "\r\n" .
-    ''
+    '<p>Bonjour,</p>' .
+    '<p>Un message a été envoyé depuis le formulaire du portfolio, le voici : </p>' .
+    "<br/>" .
+    '<hr/>' .
+    '<div><b>De la part de :</b> '.$_POST['nom'].' <'.$_POST['email'].'></div>' .
+    '<div><b>Date d\'envoi :</b> '.date("\l\\e d/m/Y à H:i:s").'</div>' .
+    '<div><b>Sujet :</b> '.$_POST['sujet'].'</div>' .
+    '<div><b>Message :</b> <p>'.str_replace(["\n", "\r\n"], '<br/>', $_POST['message']).'</p></div>' .
+    '<hr/>' .
+    "<br/>" .
+    '<p>Bonne journée.</p>'
 ;
 
 $headers =
     'From: bonjour@yohansalamone.com' . "\r\n" .
     'Reply-To: '. $_POST['nom'].'<'.$_POST['email'].'>' . "\r\n" .
-    'Content-Type: text/plain; charset="UTF-8"' . "\r\n" .
+    'Content-Type: text/html; charset="UTF-8"' . "\r\n" .
     'X-Mailer: PHP/' . phpversion();
 
 $sent = mail($to, $subject, $message, $headers);
